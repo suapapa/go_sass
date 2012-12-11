ececa758fdaadf302f90629d904aab5aa62ae792
Homin Lee <suapapa@insignal.co.kr>
2012-12-11 17:35:45 +0900

    Add wrapper for compile file
    Comment out codes to compile_folder. It is not implemented in libsass.
    And, raise compile error.

--- libsass_wrapper.c
+++ libsass_wrapper.c
@@ -2,16 +2,51 @@
 
 sass_context_t* _sass_new_context(void)
 {
-	return (sass_context_t*)sass_new_context();
+    return (sass_context_t*) sass_new_context();
 }
+
+sass_file_context_t* _sass_new_file_context(void)
+{
+    return (sass_file_context_t*) sass_new_file_context();
+}
+
+#if 0
+sass_folder_context_t* _sass_new_folder_context(void)
+{
+    return (sass_folder_context_t*) sass_new_folder_context();
+}
+#endif
 
 void _sass_free_context(sass_context_t* ctx)
 {
-	sass_free_context(ctx);
+    sass_free_context(ctx);
 }
+
+void _sass_free_file_context(sass_file_context_t* ctx)
+{
+    sass_free_file_context(ctx);
+}
+
+#if 0
+void _sass_free_folder_context(sass_folder_context_t* ctx)
+{
+    sass_free_folder_context(ctx);
+}
+#endif
 
 int _sass_compile(sass_context_t* ctx)
 {
-	return sass_compile(ctx);
+    return sass_compile(ctx);
 }
 
+int _sass_compile_file(sass_file_context_t* ctx)
+{
+    return sass_compile_file(ctx);
+}
+
+#if 0
+int _sass_compile_folder(sass_folder_context_t* ctx)
+{
+    return sass_compile_folder(ctx);
+}
+#endif
--- libsass_wrapper.h
+++ libsass_wrapper.h
@@ -3,9 +3,22 @@
 #include "sass_interface.h"
 
 typedef struct sass_options sass_options_t;
-typedef struct sass_context sass_context_t;
 
+typedef struct sass_context sass_context_t;
 sass_context_t* _sass_new_context(void);
 void _sass_free_context(sass_context_t* ctx);
 int _sass_compile(sass_context_t* ctx);
+
+typedef struct sass_file_context sass_file_context_t;
+sass_file_context_t* _sass_new_file_context(void);
+void _sass_free_file_context(sass_file_context_t* ctx);
+int _sass_compile_file(sass_file_context_t* ctx);
+
+#if 0 // compile_folder is not implemented in libsass
+typedef struct sass_folder_context sass_folder_context_t;
+sass_folder_context_t* _sass_new_folder_context(void);
+void _sass_free_folder_context(sass_folder_context_t* ctx);
+int _sass_compile_folder(sass_folder_context_t* ctx);
+#endif
+
 #endif /* __LIBSASS_WRAPPER_H__ */
