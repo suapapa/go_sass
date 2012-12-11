a3c406a3397b814a143817910ed1c248f8da0528
Homin Lee <suapapa@insignal.co.kr>
2012-12-11 20:03:34 +0900

    libsass_wrapper.c: group functions by input

--- libsass_wrapper.c
+++ libsass_wrapper.c
@@ -5,21 +5,19 @@ sass_context_t* _sass_new_context(void)
     return (sass_context_t*) sass_new_context();
 }
 
-sass_file_context_t* _sass_new_file_context(void)
-{
-    return (sass_file_context_t*) sass_new_file_context();
-}
-
-#if 0
-sass_folder_context_t* _sass_new_folder_context(void)
-{
-    return (sass_folder_context_t*) sass_new_folder_context();
-}
-#endif
-
 void _sass_free_context(sass_context_t* ctx)
 {
     sass_free_context(ctx);
+}
+
+int _sass_compile(sass_context_t* ctx)
+{
+    return sass_compile(ctx);
+}
+
+sass_file_context_t* _sass_new_file_context(void)
+{
+    return (sass_file_context_t*) sass_new_file_context();
 }
 
 void _sass_free_file_context(sass_file_context_t* ctx)
@@ -27,24 +25,24 @@ void _sass_free_file_context(sass_file_context_t* ctx)
     sass_free_file_context(ctx);
 }
 
-#if 0
-void _sass_free_folder_context(sass_folder_context_t* ctx)
-{
-    sass_free_folder_context(ctx);
-}
-#endif
-
-int _sass_compile(sass_context_t* ctx)
-{
-    return sass_compile(ctx);
-}
-
 int _sass_compile_file(sass_file_context_t* ctx)
 {
+    /* ctx->options.image_path = "images"; */
+    /* ctx->options.include_paths = "scss"; */
     return sass_compile_file(ctx);
 }
 
 #if 0
+sass_folder_context_t* _sass_new_folder_context(void)
+{
+    return (sass_folder_context_t*) sass_new_folder_context();
+}
+
+void _sass_free_folder_context(sass_folder_context_t* ctx)
+{
+    sass_free_folder_context(ctx);
+}
+
 int _sass_compile_folder(sass_folder_context_t* ctx)
 {
     return sass_compile_folder(ctx);
