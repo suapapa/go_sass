7a9c0cf89c2f8bb4d4429a1518bc3e16aef14031
Homin Lee <suapapa@insignal.co.kr>
2012-12-11 20:02:33 +0900

    sass.go: Add CompileFile()

--- sass.go
+++ sass.go
@@ -9,11 +9,10 @@ package sass
 import "C"
 import "errors"
 import "fmt"
+import "log"
 
 type Sass struct {
-	/* context C.sass_context */
 	options C.sass_options_t
-	/* options C.sass_options */
 }
 
 func NewSass() (sass *Sass, err error) {
@@ -22,27 +21,68 @@ func NewSass() (sass *Sass, err error) {
 	return
 }
 
-func (c *Sass) Compile(source string) (string, error) {
-	context, err := C._sass_new_context()
+func (c *Sass) Compile(source []byte) ([]byte, error) {
+	css, err := c.CompileToString(string(source))
+	return []byte(css), err
+}
+
+func (c *Sass) CompileToString(source string) (string, error) {
+	ctx, err := C._sass_new_context()
 	if err != nil {
-		e := errors.New("failed to make context for compile:" + err.Error())
-		return "", e
+		errStr := "failed to alloc ctx: " + err.Error()
+		return "", errors.New(errStr)
 	}
-	defer C._sass_free_context(context)
+	defer C._sass_free_context(ctx)
 
-	/* context.options = c.options */
-	context.source_string = C.CString(source)
+	// TODO: assign options!
+	// ctx.options = c.options
+	ctx.source_string = C.CString(source)
 
-	_, err = C._sass_compile(context)
+	_, err = C._sass_compile(ctx)
 	if err != nil {
-		e := errors.New("failed to compile:" + err.Error())
-		return "", e
+		errStr := "failed to compile: " + err.Error()
+		return "", errors.New(errStr)
 	}
 
-	if context.error_status != 0 {
-		e := errors.New(fmt.Sprintf("failed to compile: %d", context.error_status))
-		return "", e
+	if ctx.error_status != 0 {
+		errStr := fmt.Sprintf("failed to compile: %d: %s",
+			ctx.error_status, C.GoString(ctx.error_message))
+		return "", errors.New(errStr)
 	}
 
-	return C.GoString(context.output_string), nil
+	return C.GoString(ctx.output_string), nil
+}
+
+func (c *Sass) CompileFile(path string) ([]byte, error) {
+	css, err := c.CompileFileToString(path)
+	return []byte(css), err
+}
+
+func (c *Sass) CompileFileToString(path string) (string, error) {
+	ctx, err := C._sass_new_file_context()
+	if err != nil {
+		errStr := "failed to alloc file ctx: " + err.Error()
+		return "", errors.New(errStr)
+	}
+	defer C._sass_free_file_context(ctx)
+
+	/* ctx.options = c.options */
+	ctx.input_path = C.CString(path)
+
+	_, err = C._sass_compile_file(ctx)
+	if err != nil {
+		// TODO: don't know why error here when scss has import.
+		// even it came here, the ctx has no error and output is good.
+		errStr := "failed to compile file: " + err.Error()
+		log.Println("Can we ignore this?: " + errStr)
+		// return "", errors.New(errStr)
+	}
+
+	if ctx.error_status != 0 {
+		errStr := fmt.Sprintf("failed to compile file: %d: %s",
+			ctx.error_status, C.GoString(ctx.error_message))
+		return "", errors.New(errStr)
+	}
+
+	return C.GoString(ctx.output_string), nil
 }
